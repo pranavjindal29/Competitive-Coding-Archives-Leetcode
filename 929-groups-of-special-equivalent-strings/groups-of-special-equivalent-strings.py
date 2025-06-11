4899ff21b9771696e1d75faa4462c45bbda71e2e
Pranav Jindal <123830703+pranavjindal29@users.noreply.github.com>
2025-06-11 11:54:56 +0530

    Time: 6 ms (48.86%) | Memory: 17.8 MB (98.30%) - LeetSync

---
+++ 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numSpecialEquivGroups(self, A: List[str]) -> int:
+        
+        signature = set()
+        
+        for idx, s in enumerate(A):
+            signature.add( ''.join( sorted( s[::2] ) ) + ''.join( sorted( s[1::2] ) )  )
+        
+        return len( signature )
