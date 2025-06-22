0392b01cd45a6cb894d3600103e3bdf4f7acc9af
Pranav Jindal <123830703+pranavjindal29@users.noreply.github.com>
2025-06-22 21:30:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (90.07%) - LeetSync

---
+++ 1048-clumsy-factorial/clumsy-factorial.py
@@ -0,0 +1,20 @@
+class Solution:
+    def clumsy(self, n: int) -> int:
+        if n==4:
+            return 7
+        elif n==1:
+            return 1
+        elif n==2:
+            return 2
+        elif n==3:
+            return 6
+        if n%4==0:
+            return (n+1)
+        elif n%4==1:
+            return (n+2)
+        elif n%4==2:
+            return (n+2)
+            
+        else:
+            return (n-1)
+    
