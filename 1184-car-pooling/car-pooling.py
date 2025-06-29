00ccb6bc0589947398bb07ab96f37d1a8a258ffb
Pranav Jindal <123830703+pranavjindal29@users.noreply.github.com>
2025-06-29 13:29:53 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (93.65%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        locations = [0] * 1001
+        for numPassengers, start, end in trips:
+            locations[start] += numPassengers
+            locations[end] -= numPassengers
+        
+        for numPassengers in locations:
+            capacity -= numPassengers
+            if capacity < 0: return False
+        
+        return True
