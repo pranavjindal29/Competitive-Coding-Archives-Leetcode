e4cf93c67e4d6e997a9a49b67f0cbf16d60f612f
Pranav Jindal <123830703+pranavjindal29@users.noreply.github.com>
2025-08-01 22:22:03 +0530

    Time: 111 ms (52.97%) | Memory: 28.9 MB (88.61%) - LeetSync

---
+++ 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getStrongest(self, arr: List[int], k: int) -> List[int]:
+        arr.sort()
+        mid = arr[(len(arr)-1)//2]
+        ans = []
+        l ,r = 0, len(arr)-1
+        while(l <= r):
+            if abs(arr[l] - mid) > abs(arr[r]-mid)  :
+                ans.append(arr[l])
+                l+=1
+            else:
+                ans.append(arr[r])
+                r-=1
+        return ans[:k]
