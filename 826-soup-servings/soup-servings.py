abefb5ea5bb7c48391d1e949ae0d0125e7c87336
Pranav Jindal <123830703+pranavjindal29@users.noreply.github.com>
2025-08-08 22:50:48 +0530

    Time: 8 ms (70.30%) | Memory: 19.5 MB (64.03%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -1,30 +1,25 @@
+from functools import lru_cache
+import math
+
 class Solution:
-    def __init__(self):
-        self.optionA = [100, 75, 50, 25]
-        self.optionB = [0, 25, 50, 75]
-
-    def solve(self, a, b, dp):
-        if a == 0 and b == 0:
-            return 0.5  # half the probability that A and B become empty at the same time
-        if a == 0:
-            return 1
-        if b == 0:
-            return 0
-
-        if dp[a][b] != -1:
-            return dp[a][b]
-
-        ans = 0
-        for k in range(4):
-            rema = a - self.optionA[k]
-            remb = b - self.optionB[k]
-            ans += 0.25 * self.solve(max(0, rema), max(0, remb), dp)
-
-        dp[a][b] = ans
-        return ans
-
     def soupServings(self, n: int) -> float:
-        if n >= 4800:
-            return 1
-        dp = [[-1] * (n + 1) for _ in range(n + 1)]
-        return self.solve(n, n, dp)
+        if n > 5000:
+            return 1.0
+        units = math.ceil(n / 25)
+
+        @lru_cache(None)
+        def calc_prob(soupA, soupB):
+            if soupA <= 0 and soupB <= 0:
+                return 0.5
+            if soupA <= 0:
+                return 1.0
+            if soupB <= 0:
+                return 0.0
+            return 0.25 * (
+                calc_prob(soupA - 4, soupB) +
+                calc_prob(soupA - 3, soupB - 1) +
+                calc_prob(soupA - 2, soupB - 2) +
+                calc_prob(soupA - 1, soupB - 3)
+            )
+
+        return calc_prob(units, units)
