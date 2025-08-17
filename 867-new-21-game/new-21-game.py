ef63ca7ee8f1964ee4425ea5086b7a142c58269d
Pranav Jindal <123830703+pranavjindal29@users.noreply.github.com>
2025-08-17 12:08:25 +0530

    Time: 23 ms (72.90%) | Memory: 18 MB (95.48%) - LeetSync

--- 867-new-21-game/new-21-game.py
+++ 867-new-21-game/new-21-game.py
@@ -1,23 +1,25 @@
 class Solution:
     def new21Game(self, n: int, k: int, maxPts: int) -> float:
-        if k == 0 or n >= k + maxPts:
+        if k == 0 or n >= k - 1 + maxPts:
             return 1.0
-        
-        windowSum = 1.0
-        probability = 0.0
-        
-        dp = [0.0] * (n + 1)
+
+        dp = [0.0] * maxPts
         dp[0] = 1.0
-        
+
+        window_sum = 1.0
+        result = 0.0
+
         for i in range(1, n + 1):
-            dp[i] = windowSum / maxPts
-            
+            prob = window_sum / maxPts
+
             if i < k:
-                windowSum += dp[i]
+                window_sum += prob
             else:
-                probability += dp[i]
-            
+                result += prob
+
             if i >= maxPts:
-                windowSum -= dp[i - maxPts]
-        
-        return probability
+                window_sum -= dp[i % maxPts]
+
+            dp[i % maxPts] = prob
+
+        return result
