8958ed8c7cd08d02890fa5883ada426ffaed124b
Pranav Jindal <123830703+pranavjindal29@users.noreply.github.com>
2025-09-14 23:00:17 +0530

    Time: 46 ms (42.78%) | Memory: 20.3 MB (55.81%) - LeetSync

--- 1006-vowel-spellchecker/vowel-spellchecker.py
+++ 1006-vowel-spellchecker/vowel-spellchecker.py
@@ -1,23 +1,28 @@
 class Solution:
     def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
-        orig = set(wordlist) # original words O(1) lookup 
-        case = {} # diff in case
-        vowel = {} # diff in vowel
-        
-        for word in wordlist: 
-            key = word.lower()
-            case.setdefault(key, []).append(word)
-            for c in "aeiou": key = key.replace(c, "*")
-            vowel.setdefault(key, []).append(word)
-        
-        ans = []
-        for word in queries: 
-            if word in orig: ans.append(word)
-            else: 
-                key = word.lower()
-                if key in case: ans.append(case[key][0])
-                else: 
-                    for c in "aeiou": key = key.replace(c, "*")
-                    if key in vowel: ans.append(vowel[key][0])
-                    else: ans.append("")
-        return ans 
+        original = set(wordlist)
+        caseMap = {}
+        vowelMap = {}
+
+        def changeVowel(word: str) -> str:
+            return "".join("*" if ch in "aeiou" else ch for ch in word)
+
+        for w in wordlist:
+            lower = w.lower()
+            caseMap.setdefault(lower, w)
+            vowelMap.setdefault(changeVowel(lower), w)
+
+        res = []
+        for q in queries:
+            lower = q.lower()
+            cv = changeVowel(lower)
+
+            if q in original:
+                res.append(q)
+            elif lower in caseMap:
+                res.append(caseMap[lower])
+            elif cv in vowelMap:
+                res.append(vowelMap[cv])
+            else:
+                res.append("")
+        return res
